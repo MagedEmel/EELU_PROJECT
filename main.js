7968841ac31e24b76e1f30c9e4c8f277944dadd9
MagedEmel <magedemel007@gmail.com>
2026-03-21 22:48:31 +0200

    s

--- main.js
+++ main.js
@@ -2,11 +2,23 @@
 
 let toMenu = document.getElementById("toMenu");
 let sideBar = document.getElementById("sideBar");
+
 toMenu.onclick = () => {
   sideBar.classList.toggle("active");
 };
+
 window.addEventListener("resize", function () {
-    if (window.innerWidth >= 1270 && sideBar.classList.contains("active")) {
-        sideBar.classList.remove("active");
-    }
+  if (window.innerWidth >= 1270) {
+    sideBar.classList.remove("active");
+  }
+});
+
+document.addEventListener("click", (e) => {
+  if (
+    sideBar.classList.contains("active") &&
+    !sideBar.contains(e.target) &&
+    !toMenu.contains(e.target)
+  ) {
+    sideBar.classList.remove("active");
+  }
 });
